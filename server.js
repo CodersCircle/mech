9e24ca63ed27840afcdc1efe30db3d72ab25bc87
BABA <allymechservices@gmail.com>
2026-02-28 06:08:09 +0530

    realtime server

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+let onlineWorkers = {};
+
+// worker connects
+io.on("connection", (socket) => {
+    console.log("Connected:", socket.id);
+
+    socket.on("worker-online", (workerId) => {
+        onlineWorkers[workerId] = socket.id;
+        console.log("Worker online:", workerId);
+    });
+
+    socket.on("disconnect", () => {
+        for (let id in onlineWorkers) {
+            if (onlineWorkers[id] === socket.id) {
+                delete onlineWorkers[id];
+            }
+        }
+    });
+
+    socket.on("new-booking", (bookingData) => {
+        Object.values(onlineWorkers).forEach((workerSocket) => {
+            io.to(workerSocket).emit("booking-request", bookingData);
+        });
+    });
+
+    socket.on("accept-booking", (data) => {
+        io.emit("booking-accepted", data);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Realtime Server Running");
+});
+
+server.listen(3000, () => {
+    console.log("Server started");
+});
